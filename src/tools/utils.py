1f642863aecf8ffd11db7cd915753e6451770fd0
Sean <abstractSean.py@gmail.com>
2018-01-31 21:31:34 -0800

    add(utils load_data): added function for loading data into data from file

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -1,4 +1,13 @@
 
+import os
+import pandas as pd
+import sys
+sys.path.append('/home/sean/Projects/numerai/numerai')
+
+from src.data import get_raw_data
+from src.tools import numerai_api
+
+
 def create_submission(df_predict_features, model, filename='predictions.csv'):
     df_predict_features['probability'] = model.predict_proba(
                                                 df_predict_features)[:,1]
@@ -16,3 +25,14 @@ def get_validation_log_loss(df, model):
     df_val_prediction = model.predict_proba(df_val_predict_feat)
     return log_loss(df_val_target, df_val_prediction)
     
+
+def load_data():
+    round_number = numerai_api.get_current_round()
+    raw_data_path = os.path.join(os.path.pardir, os.path.pardi, 'data','raw')
+    raw_data_file = os.path.join(raw_data_path, '{}_numerai_raw.pkl'.format(round_number))
+
+    try:
+        return pd.read_pickle(raw_data_file)
+    except FileNotFoundError:
+        get_raw_data.main()
+        return pd.read_pickle(raw_data_file)
