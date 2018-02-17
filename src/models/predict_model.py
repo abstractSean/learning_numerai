9dce9ab563339ca8dca08d409c1ae0ffdb06d494
Sean <abstractSean.py@gmail.com>
2018-02-16 23:50:04 -0800

    fixes(predict_model): fixed errors passing limited features into model predict and other small changes/fixes

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -11,20 +11,21 @@ from numerapi import numerapi
 
 from src.tools import numerai_api, utils
 from src.models import train_RFC
-
+from src.models.manage_model_files import *
 
 def predict_with_noise(df, model, features, noise=0.0):
     df_predict = df.loc[(df['data_type'] == 'validation') |
                         (df['data_type'] == 'test') |
                         (df['data_type'] == 'live'), :]
-    df_features = df_predict.iloc[:,features]
-    df_predict.loc[:,'probability'] = (model.predict_proba(df_features)[:,1]
-                                       + random.uniform(-noise,noise)
-                                      )
+    drop_columns = ['era','data_type', 'target']
+    df_features = df_predict.drop(drop_columns, axis=1).iloc[:,features]
+    df_predict.loc[:,'probability'] = model.predict_proba(df_features)[:,1]
+    df_predict.loc[:,'probability'] = df_predict.loc[:,'probability'] + random.uniform(-noise,noise)
+                                      
     return df_predict
 
 def create_submission_file(df, filename):
-    df['id'] = df.index
+    df.loc[:,'id'] = df.index
     df = df.loc[:, ['id','probability']]
     df.to_csv(filename, index=False)
 
@@ -37,24 +38,26 @@ def get_napi():
     
     return numerapi.NumerAPI(public_id, secret_key, verbosity='info')
 
-def main():
+def main(noise=0.0):
     logger.info('Get data')
     df = utils.load_data()
     
-    X_train = (df.loc[df['data_type']=='train', :]
-                 .drop(['era','data_type','target'], axis=1)
-              )
+    X_train = df.loc[df['data_type']=='train','feature1':'feature50']
     y_train = df.loc[df['data_type']=='train', 'target'] 
 
     logger.info('Train model')
-    model, features = train_RFC.train_rfc_filtered(X_train, y_train)
+    try:
+        model, features = load_model('rfc_filtered') 
+    except FileNotFoundError:
+        model, features = train_RFC.train_rfc_filtered(X_train, y_train)
+        save_model((model, features),'rfc_filtered')
 
     logger.info('Predict')
-    df_predict = predict_with_noise(df, model, features, 0.0)
+    df_predict = predict_with_noise(df, model, features, float(noise))
 
     round_number = numerai_api.get_current_round()
     filename = 'predictions_RFC_{}.csv'.format(round_number)
-
+    logger.info('Create submission')
     create_submission_file(df_predict, filename)
 
     napi = get_napi()
@@ -68,6 +71,8 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     logger = logging.getLogger()
-    main()
-
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        main()
 
