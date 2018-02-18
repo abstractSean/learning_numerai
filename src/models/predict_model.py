8532e4e76635ba7d384abc88710bdf8de4d1be42
Sean <abstractSean.py@gmail.com>
2018-02-17 23:34:48 -0800

    added(get_napi): input for username

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -28,11 +28,11 @@ def create_submission_file(df, filename):
     df.to_csv(filename, index=False)
 
 
-def get_napi():
+def get_napi(user='NUMERAI'):
     dotenv_path = find_dotenv()
     load_dotenv(dotenv_path)
-    public_id = os.environ.get('NUMERAI_SUBMIT_ID')
-    secret_key = os.environ.get('NUMERAI_SUBMIT_KEY')
+    public_id = os.environ.get('{}_SUBMIT_ID'.format(user))
+    secret_key = os.environ.get('{}_SUBMIT_KEY'.format(user))
     
     return numerapi.NumerAPI(public_id, secret_key, verbosity='info')
 
