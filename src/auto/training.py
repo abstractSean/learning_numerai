fa0d7ce23907a98a70b1b12d2ea53aa6c5c81f11
Sean <abstractSean.py@gmail.com>
2018-05-09 22:39:26 -0700

    fix(training): added ensemble transition

--- src/auto/training.py
+++ src/auto/training.py
@@ -7,3 +7,7 @@ class Training(AbsState):
     
     def check(self):
         self._model.state = self._model.checking
+
+    def ensemble(self):
+        self._model.state = self._model.ensembling
+
