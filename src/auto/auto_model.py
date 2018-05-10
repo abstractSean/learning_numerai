4ce459d50104d40b281f0b10e6f431106be88e9f
Sean <abstractSean.py@gmail.com>
2018-05-09 22:22:35 -0700

    add(auto): filled out AutoModel class with states

--- src/auto/auto_model.py
+++ src/auto/auto_model.py
@@ -1,10 +1,24 @@
 from waiting import Waiting
 from getting_data import GettingData
+from training import Training
+from checking import Checking
+from ensembling import Ensembling
+from preparing import Preparing
+from submitting import Submitting
+from staking import Staking
+
 
 class AutoModel:
     def __init__(self):
         self.waiting = Waiting(self)
         self.getting_data = GettingData(self)
+        self.training = Training(self)
+        self.checking = Checking(self)
+        self.ensembling = Ensembling(self)
+        self.preparing = Preparing(self)
+        self.submitting = Submitting(self)
+        self.staking = Staking(self)
+
         self.state = self.waiting
 
     def wait(self):
@@ -27,6 +41,9 @@ class AutoModel:
 
     def submit(self):
         self.state.submit()
+    
+    def stake(self):
+        self.state.stake()
 
     def randomize(self):
         self.state.randomize()
