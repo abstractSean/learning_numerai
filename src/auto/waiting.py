55b201fcedd44371023f821ffb6969d223d48ae4
Sean <abstractSean.py@gmail.com>
2018-05-11 23:17:45 -0700

    chore(waiting): added length for sleep and get_data call

--- src/auto/waiting.py
+++ src/auto/waiting.py
@@ -9,10 +9,11 @@ class Waiting(AbsState):
 
     def get_data(self):
         self._model.state = self._model.getting_data
+        self._model.get_data()
 
-    def wait(self):
+    def wait(self, seconds=5):
         print('Waiting for new round')
-        time.sleep(5)
+        time.sleep(seconds)
         
         if self._model.napi.check_new_round():
             self.get_data()
