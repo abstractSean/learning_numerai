c6c7b1235b00f2fb80f04bd4a44d7538e6545c6b
Sean <abstractSean.py@gmail.com>
2018-05-14 20:35:24 -0700

    add(getting_data): started filling in get_data

--- src/auto/getting_data.py
+++ src/auto/getting_data.py
@@ -1,6 +1,21 @@
 from abs_state import AbsState
+from src.tools import utils
 
 class GettingData(AbsState):
 
+    def get_data(self):
+        self._model.logger.info('Getting data.')
+        self._model.df = utils.load_data()
+
+        self._model.X_train = self._model.df.loc[
+                                self._model.df['data_type']=='train',
+                                'feature1':'feature50']
+        self._model.y_train = self._model.df.loc[
+                    self._model.df['data_type']=='train',
+                    'target']
+
+
     def train(self):
         self._model.state = self._model.training
+
+
