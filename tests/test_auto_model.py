814683ab9b346c383cd46a60de1654184df3ddb5
Sean <abstractSean.py@gmail.com>
2018-05-15 22:41:15 -0700

    fix(test): fixed up first test

--- tests/test_auto_model.py
+++ tests/test_auto_model.py
@@ -1,7 +1,12 @@
+import pytest
+
 from src import AutoModel
 from src.auto import Waiting
 
-
-def test_AutoModel():
+@pytest.fixture(scope='function', name='auto')
+def auto_fixture():
     auto = AutoModel()
+    return auto
+
+def test_AutoModel(auto):
     assert type(auto.state) == type(Waiting(None))
