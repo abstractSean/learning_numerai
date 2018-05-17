3dd96cb6f3ea8522a9353fd79f631e3765a7e56b
Sean <abstractSean.py@gmail.com>
2018-05-16 21:06:35 -0700

    change(waiting): changed wait to auto_wait to distinguish from state transition

--- src/auto/waiting.py
+++ src/auto/waiting.py
@@ -12,12 +12,12 @@ class Waiting(AbsState):
         self._model.state = self._model.getting_data
         self._model.get_data()
 
-    def wait(self, seconds=5):
+    def auto_wait(self, seconds=5):
         print('Waiting for new round')
         time.sleep(seconds)
         
         if self._model.napi.check_new_round():
             self.get_data()
         else:
-            self.wait()
+            self.auto_wait()
 
