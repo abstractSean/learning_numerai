5efb512a9e5a74b87e7d81040fc8c9a67019abc2
Sean <abstractSean.py@gmail.com>
2018-05-16 22:36:42 -0700

    add(training): started filling in trainig functions

--- src/auto/training.py
+++ src/auto/training.py
@@ -1,13 +1,23 @@
+from src.models import train_RFC
+from src.models.manage_model_files import load_model, save_model
+
 from .abs_state import AbsState
 
 class Training(AbsState):
 
     def train(self):
+        model = self._model
+        model.logger.info('Training model')
+
+        if not model.test:
+            try:
+                model, features = load_model('rfc_filtered')
+            except FileNotFoundError:
+                model, features = train_RFC.train_rfc_filtered(
+                                                X_train, y_train)
+                save_model((model, features),'rfc_filtered')
+                                        
+
         self._model.state = self._model.checking
     
-    def check(self):
-        self._model.state = self._model.checking
-
-    def ensemble(self):
-        self._model.state = self._model.ensembling
 
