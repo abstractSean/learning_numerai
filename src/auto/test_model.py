02678a15a55a7c22a9feff70851c0fe381481d72
Sean <abstractSean.py@gmail.com>
2018-05-18 22:02:15 -0700

    add(test_model): short test script for testing models after submitting current week

---
+++ src/auto/test_model.py
@@ -0,0 +1,39 @@
+import logging
+import sched, time
+
+from src.auto.auto_model import AutoModel
+
+
+day_length = 86400  # seconds
+week_length = 7     # days
+
+
+def wait_loop(model, seconds):
+    if model.check():
+        model.get_data()
+    else:
+        logger.info('Waiting for new round')
+        time.sleep(seconds)
+        wait_loop()
+
+
+def main():
+    model = AutoModel()
+
+    #wait_loop(model, 10)
+    model.stop_waiting()
+    model.get_data()
+    model.train()
+    model.predict()
+    model.check()
+    #model.prepare()
+    #model.submit()
+    #model.stake()
+
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger()
+    main()
